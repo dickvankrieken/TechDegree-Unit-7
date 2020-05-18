821f38c9757de4a5ec3bb150e5f9f93d4d9962e1
Dick van Krieken <dickvankrieken@outlook.com>
2020-05-18 16:42:10 +0200

    working on layout

--- js/chart.js
+++ js/chart.js
@@ -2,9 +2,12 @@
 
 const trafficOptions = document.querySelectorAll('.traffic-option-link');
 
+const dailyCanvas = document.getElementById("daily-chart");
+
+const mobileCanvas = document.getElementById("mobile-chart");
+
 // Hourly traffic chart
 
-let hourlyTrafficCanvas = document.getElementById('myChart');
 
 
 let hourlyTrafficData = {
@@ -20,7 +23,6 @@ let hourlyTrafficData = {
 // Daily traffic chart
 
 let dailyTrafficCanvas = document.getElementById('myChart');
-
 
 let dailyTrafficData = {
     labels: ["20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"],
@@ -83,6 +85,12 @@ let monthlyTrafficData = {
 
 // Click listener for daily option
 trafficOptions[1].addEventListener('click', ()=> {
+    $('#myChart').remove();
+    $('#line-chart-canvas-container').append('<canvas id="myChart"></canvas>');
+
+    let dailyTrafficCanvas = document.getElementById('myChart');
+
+
     let dailyTrafficChart = new Chart(dailyTrafficCanvas, {
         type: 'line',
         data: dailyTrafficData,
@@ -99,6 +107,12 @@ trafficOptions[1].addEventListener('click', ()=> {
 
 // Click listener for hourly option
 trafficOptions[0].addEventListener('click', ()=> {
+    $('#myChart').remove();
+    $('#line-chart-canvas-container').append('<canvas id="myChart"></canvas>');
+
+    let hourlyTrafficCanvas = document.getElementById('myChart');
+
+
     let hourlyTrafficChart = new Chart(hourlyTrafficCanvas, {
         type: 'line',
         data: hourlyTrafficData,
@@ -116,6 +130,16 @@ trafficOptions[0].addEventListener('click', ()=> {
 
 // Click listener for weekly option
 trafficOptions[2].addEventListener('click', ()=> {
+    // weeklyTrafficChart.destroy();
+    // monthlyTrafficChart.destroy();
+    // hourlyTrafficChart.destroy();
+    // dailyTrafficChart.destroy();
+
+    $('#myChart').remove();
+    $('#line-chart-canvas-container').append('<canvas id="myChart"></canvas>');
+
+    let weeklyTrafficCanvas = document.getElementById('myChart');
+
     let weeklyTrafficChart = new Chart(weeklyTrafficCanvas, {
         type: 'line',
         data: weeklyTrafficData,
@@ -133,6 +157,12 @@ trafficOptions[2].addEventListener('click', ()=> {
 
 // Click listener for montly option
 trafficOptions[3].addEventListener('click', ()=> {
+    $('#myChart').remove();
+    $('#line-chart-canvas-container').append('<canvas id="myChart"></canvas>');
+
+    let monthlyTrafficCanvas = document.getElementById('myChart');
+
+    
     let monthlyTrafficChart = new Chart(monthlyTrafficCanvas, {
         type: 'line',
         data: monthlyTrafficData,
@@ -146,3 +176,87 @@ trafficOptions[3].addEventListener('click', ()=> {
     trafficOptions[0].classList.remove('active');
     trafficOptions[2].classList.remove('active');
 });
+
+
+// Draw the weekly chart for the first time before anything is clicked
+let weeklyTrafficChart = new Chart(weeklyTrafficCanvas, {
+    type: 'line',
+    data: weeklyTrafficData,
+    options: allTrafficOptions
+});
+
+// data for daily traffic bar chart
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+    label: '# of Hits',
+    data: [75, 115, 175, 125, 225, 200, 100],
+    backgroundColor: '#7477BF',
+    borderWidth: 1
+    }]
+};
+
+// options for daily traffic bar chart
+const dailyOptions = {
+    responsive: true,   
+    maintainAspectRatio: false, 
+    scales: {
+    yAxes: [{
+    ticks: {
+    beginAtZero:true
+    }
+    }]
+    },
+    legend : {
+    display: false
+    }
+}
+
+// draw the daily traffic bar chart
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+
+// data for the mobile data doughnut chart
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+    label: '# of Users',
+    data: [2000, 550, 500],
+    borderWidth: 0,
+    backgroundColor: [
+    '#7477BF',
+    '#78CF82',
+    '#51B6C8'
+    ]
+    }]
+};
+
+// options for mobile data doughnut chart
+
+const mobileOptions = {
+    responsive: true,   
+    maintainAspectRatio: false, 
+    legend: {
+    position: 'right',
+    labels: {
+    boxWidth: 20,
+    fontStyle: 'bold'
+    }
+    }
+}
+
+// draw the mobile data doughnut chart 
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    options: mobileOptions
+    });
+
+    
+    
